1d8ce4bb967d27a261e5376997659f0997c67c4f
gershon.araque <geraquem@gmail.com>
2022-06-30 10:47:25 +0200

    wip

--- app/src/main/java/com/mmfsin/bimorph/presentation/dualism/DualismFragment.kt
+++ app/src/main/java/com/mmfsin/bimorph/presentation/dualism/DualismFragment.kt
@@ -23,7 +23,6 @@ class DualismFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        
     }
 }
